660c99d6556b036064c1d1956af252f4f0f45e93
gnshvsk <zoya.gn@gmail.com>
2019-12-02 01:59:58 +0600

    Small change
    Изменения в дизайне формы

--- Weather/Form1.Designer.cs
+++ Weather/Form1.Designer.cs
@@ -56,6 +56,7 @@
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.groupBox1.Location = new System.Drawing.Point(16, 166);
             this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
@@ -81,7 +82,7 @@
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(6, 62);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(45, 16);
+            this.label7.Size = new System.Drawing.Size(57, 21);
             this.label7.TabIndex = 1;
             this.label7.Text = "label7";
             // 
@@ -90,59 +91,59 @@
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(6, 27);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(45, 16);
+            this.label6.Size = new System.Drawing.Size(57, 21);
             this.label6.TabIndex = 0;
             this.label6.Text = "label6";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(337, 87);
+            this.label5.Location = new System.Drawing.Point(334, 96);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(45, 16);
+            this.label5.Size = new System.Drawing.Size(57, 21);
             this.label5.TabIndex = 5;
             this.label5.Text = "label5";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(334, 52);
+            this.label4.Location = new System.Drawing.Point(334, 61);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(45, 16);
+            this.label4.Size = new System.Drawing.Size(57, 21);
             this.label4.TabIndex = 4;
             this.label4.Text = "label4";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(123, 50);
+            this.label3.Location = new System.Drawing.Point(123, 61);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(45, 16);
+            this.label3.Size = new System.Drawing.Size(57, 21);
             this.label3.TabIndex = 3;
             this.label3.Text = "label3";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(123, 87);
+            this.label2.Location = new System.Drawing.Point(123, 96);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(45, 16);
+            this.label2.Size = new System.Drawing.Size(57, 21);
             this.label2.TabIndex = 2;
             this.label2.Text = "label2";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(123, 13);
+            this.label1.Location = new System.Drawing.Point(123, 24);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.Size = new System.Drawing.Size(57, 21);
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
             // 
             // panel1
             // 
             this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.panel1.Location = new System.Drawing.Point(8, 13);
+            this.panel1.Location = new System.Drawing.Point(8, 27);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(90, 90);
             this.panel1.TabIndex = 0;
@@ -150,30 +151,30 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label8.Location = new System.Drawing.Point(24, 13);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(169, 24);
+            this.label8.Size = new System.Drawing.Size(177, 22);
             this.label8.TabIndex = 2;
             this.label8.Text = "Местоположение";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label9.Location = new System.Drawing.Point(210, 13);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(63, 24);
+            this.label9.Size = new System.Drawing.Size(63, 23);
             this.label9.TabIndex = 3;
             this.label9.Text = "Omsk";
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label10.Location = new System.Drawing.Point(24, 64);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(261, 24);
+            this.label10.Size = new System.Drawing.Size(272, 22);
             this.label10.TabIndex = 4;
             this.label10.Text = "Изменить местоположение";
             // 
@@ -186,6 +187,7 @@
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button1.Location = new System.Drawing.Point(311, 107);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
@@ -197,6 +199,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.ClientSize = new System.Drawing.Size(626, 427);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.SelectCity);
@@ -207,7 +210,7 @@
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Погода";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
